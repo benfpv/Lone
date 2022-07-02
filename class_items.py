f9072298f323eda732d0ddc0aa398c13327225a1
Ben <d3boy2013@gmail.com>
2022-07-02 19:59:06 -0400

    add itemsList and import
    add itemsList and import

---
+++ class_items.py
@@ -0,0 +1,9 @@
+class ItemsList:
+    def __init__(self, id, name, tags, description, dateOfManufacture, dimensions, weight):
+        self.id = id
+        self.name = name
+        self.tags = tags
+        self.description = description
+        self.dateOfManufacture = dateOfManufacture
+        self.dimensions = dimensions
+        self.weight = weight
---
+++ import_items.py
@@ -0,0 +1,24 @@
+import csv
+
+from class_items import *
+
+def import_itemsList():
+    itemsList = [];
+    with open('itemsList.csv', newline='') as csvfile:
+        itemsList_csv = csv.reader(csvfile, delimiter=',');
+        row_count = 0;
+        for row in itemsList_csv:
+            itemsList.append([str(row_count)]);
+            for i in row:
+                itemsList[row_count].append(i);
+            row_count += 1;
+    return itemsList;
+
+def objectify_itemsList(itemsList):
+    itemsListObj = [];
+    for row in itemsList:
+        itemsListObj.append(ItemsList(row[0], row[1], row[2], row[3], row[4], row[5], row[6]));
+    return itemsListObj;
+
+itemsList = import_itemsList();
+itemsListObj = objectify_itemsList(itemsList);

95a1de942106647cbb40916e705c0990e728a066
Andrew Jiang <andrew.jiang@mail.utoronto.ca>
2022-07-13 20:18:07 -0400

    Added user input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from re import M
+from traceback import print_exc
 import numpy as np
 import math
 import random
@@ -11,70 +12,45 @@ from market_logic import Market
 from order import BuyOrder, SellOrder
 from user import User
 
+class Game:
+    def __init__(self):
+        self.exit = False
+        self.market = Market()
 
-def main():
-    pass
+        user1 = User(1, "Ben", {itemsList[0].name: itemsList[0]}, 1000)
+        user2 = User(2, "Jeff Bezos", {itemsList[2].id: itemsList[2]}, 1000000)
 
-def gameLoop(market):
-    command = input("What would you like to do\n")
-    command = command.lower()
-    if command.lower() == "help":
-        pass
-    elif command.lower() == "select user":
-        pass
-    elif command.lower() == "open inventory":
-        pass
-    elif command.lower() == "Buy":
-        pass
+        self.market.add_user(user1)
+        self.market.add_user(user2)
 
-    switch = {
-        "h" : lambda switch: print([k for k in switch.keys()]),
-        "select user": select_user(market)
-    }
+    def printCommands(self):
+        print("Commands:\n\t-help\n\t-select user\n\t-inventory\n\t-buy [stock] [value]\n\t-sell [stock] [value]")    
 
-    switch[command](switch)
-
-def select_user(market):
-    for user in market.users:
-        print(user)
-    name = input("Which user are you?\n")
-    while not name in market.users:
-        if name in market.users:
-            market.currentUser = market.users[name]
+    def gameLoop(self):
+        command = input("What would you like to do\n")
+        command = command.lower()
+        if command == "help":
+            self.printCommands()
+        elif command == "select user":
+            self.select_user()
         else:
-            name = input("Not a valid name, which user are you?\n")
+            print("That's not a option. Available commands are:\n")
+            self.printCommands()
+    
 
-if __name__ == '__main__':
-    market = Market()    
+    def select_user(self):
+        for user in self.market.users:
+            print(user)
+        name = input("Which user are you?\n")
+        while not name in self.market.users:
+            if name in self.market.users:
+                self.market.currentUser = self.market.users[name]
+            else:
+                name = input("Not a valid name, which user are you?\n")
 
-    user1 = User(1, "Ben", {itemsList[0].name: itemsList[0]}, 1000)
-    user2 = User(2, "Jeff Bezos", {itemsList[2].id: itemsList[2]}, 1000000)
+if __name__ == '__main__':    
 
-    market.add_user(user1)
-    market.add_user(user2)
-
-    gameLoop(market)
-    """
-    itemsListCsv = import_itemsList() 
-    itemsList = objectify_itemsList(itemsListCsv) 
-
-    print('----- debug market -----');
-    market = Market()
-
-    user1 = User(1, "Ben", {itemsList[0].name: itemsList[0]}, 1000)
-    user2 = User(2, "Jeff Bezos", {itemsList[2].id: itemsList[2]}, 1000000)
-
-    market.add_user(user1)
-    market.add_user(user2)
-
-    user1.sell(market, 100, itemsList[0])
-    market.print_orders()
-    user2.buy(market, 200, itemsList[2])
-    market.print_orders()
-    user2.buy(market, 200, itemsList[0])
-    market.print_orders()
-    user2.cancelBuy(market, user2.buyOrders[0])
-    market.print_orders()
-    print('----- debug marketHistory -----');
-    market.print_marketHistory();
-    """
+    game = Game()
+    
+    while not game.exit:
+        game.gameLoop()
